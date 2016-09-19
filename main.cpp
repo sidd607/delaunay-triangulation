66cb198d777b41d8bd9549dc3c0da037a024f625
siddartha <sidd607@gmail.com>
2016-09-19 09:42:32 +0530

    updated main

--- main.cpp
+++ main.cpp
@@ -26,20 +26,10 @@ float RandomFloat(float a, float b) {
 std::vector<Edge> global_incremental_edges;
 
 void renderFunction(){
-  //glClearColor(0.0, 0.0, 0.0, 0.0);
+  
   std::cout << "From Render Function" << std::endl;
-
   glClear(GL_COLOR_BUFFER_BIT);
-  //glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-  /*
-  glBegin(GL_LINES);
-      glVertex2f(10, 10);
-      glVertex2f(400, 400);
-  glEnd();
-  */
-
   for(auto &e : global_incremental_edges){
-    std::cout << e << std::endl;
     glBegin(GL_LINES);
       glVertex2f(e.p1.x, e.p1.y);
       glVertex2f(e.p2.x, e.p2.y);
@@ -54,13 +44,15 @@ int main(int argc, char** argv)
 {
 	srand (time(NULL));
 	float numberPoints = roundf(RandomFloat(4, 40));
-  numberPoints = 100;
+  numberPoints = 10;
+  int window_height = 800;
+  int window_width = 600;
 
 	std::cout << "Generating " << numberPoints << " random points" << std::endl;
 
 	std::vector<Vec2f> points;
 	for(int i = 0; i < numberPoints; i++) {
-		points.push_back(Vec2f(RandomFloat(0, 1000), RandomFloat(0, 1000)));
+		points.push_back(Vec2f(RandomFloat(10, window_height - 10), RandomFloat(10, window_width - 10)));
 	}
 
 	Delaunay triangulation;
@@ -84,66 +76,20 @@ int main(int argc, char** argv)
 
   global_incremental_edges = edges;
 
-
+  // openGl Drawing for Incremental Delaunay Triangulation
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE);
-  glutInitWindowSize(1000,1000);
-  glutCreateWindow("OpenGL - First window demo");
+  glutInitWindowSize(window_height,window_height);
+  glutCreateWindow("Incremental Delaunay Triangulation");
   glClearColor(0, 0, 0, 0);
-  glViewport(0, 0, 1000, 1000);
+  glViewport(0, 0, window_height, window_width);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  glOrtho(0, 1000, 0, 1000, 1, -1);
+  glOrtho(0, window_height, 0, window_width, 1, -1);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   glutDisplayFunc(renderFunction);
   glutMainLoop();
 
-
-	// SFML window
-  //sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  /*
-	// Transform each points of each vector as a rectangle
-	std::vector<sf::RectangleShape*> squares;
-
-	for(auto p = begin(points); p != end(points); p++) {
-		sf::RectangleShape *c1 = new sf::RectangleShape(sf::Vector2f(4, 4));
-		c1->setPosition(p->x, p->y);
-		squares.push_back(c1);
-	}
-
-	// Make the lines
-	std::vector<std::array<sf::Vertex, 2> > lines;
-	for(auto e = begin(edges); e != end(edges); e++) {
-		lines.push_back({{
-			sf::Vertex(sf::Vector2f((*e).p1.x + 2, (*e).p1.y + 2)),
-			sf::Vertex(sf::Vector2f((*e).p2.x + 2, (*e).p2.y + 2))
-		}});
-	}
-
-	while (window.isOpen())
-	{
-	        sf::Event event;
-	        while (window.pollEvent(event))
-	        {
-	            if (event.type == sf::Event::Closed)
-	                window.close();
-	        }
-
-	        window.clear();
-
-		// Draw the squares
-		for(auto s = begin(squares); s != end(squares); s++) {
-			window.draw(**s);
-		}
-
-		// Draw the lines
-		for(auto l = begin(lines); l != end(lines); l++) {
-			window.draw((*l).data(), 2, sf::Lines);
-		}
-
-		window.display();
-	}
-  */
 	return 0;
 }
